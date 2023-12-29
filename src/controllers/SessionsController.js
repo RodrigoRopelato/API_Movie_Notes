58e2cf8f60c344d79d6a7c1b2764155bac8c0662
Rodrigo Ropelato <rodrigoropelato@hotmail.com>
2023-12-29 15:21:49 -0300

    ajuste SessionsController referencia de tabela

--- src/controllers/SessionsController.js
+++ src/controllers/SessionsController.js
@@ -9,7 +9,7 @@ class SessionsController {
     async create(request, response){
         const {email, password} = request.body;
 
-        const user = await knex("user").where({email}).first();
+        const user = await knex("users").where({email}).first();
 
         if(!user){
             throw new AppError("Email e/ou senha incorreta",401);
