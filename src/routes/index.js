66c9db6617dc8d5bff2604e11db76c8660bcfafa
Rodrigo Ropelato <rodrigoropelato@hotmail.com>
2023-12-29 15:10:36 -0300

    implementando Sessions Controler e Sessions Routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,14 @@
 const {Router} = require("express");
 
 const usersRoutes = require("./user.routes.js");
+const sessionsRoutes = require("./sessions.routes.js");
 const movieRoutes = require("./movie.routes.js");
 const tagsRoutes = require("./tags.routes.js");
 
 const routes = Router();
 
 routes.use("/users", usersRoutes);
+routes.use("/sessions", sessionsRoutes);
 routes.use("/movie", movieRoutes);
 routes.use("/tags", tagsRoutes);
 
